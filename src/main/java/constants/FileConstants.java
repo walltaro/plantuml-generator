1a4e54e55cd377c08f4d479fb1fe24305fdd2ba3
Walltaro <wtr1110amb6@gmail.com>
2018-07-08 11:09:38 +0900

    Modify base from generate file to write file.

---
+++ src/main/java/constants/FileConstants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public final class FileConstants {
+	private FileConstants() {
+
+	}
+	/** 拡張子：PUML */
+	public static final String PUML_EXTENTION = ".puml";
+	/** 標準文字コード：UTF-8 */
+	public static final String DEFAULT_CHAR_SET = "UTF-8";
+	/** 追記モード：オフ */
+	public static final boolean NOT_APPEND = false;
+}
--- src/main/java/generator/ClassDiagramGenerator.java
+++ src/main/java/generator/ClassDiagramGenerator.java
@@ -2,36 +2,49 @@ package generator;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
+import constants.FileConstants;
+
 public class ClassDiagramGenerator implements DiagramGenerator {
-	private static final String PUML_EXTENTION = ".puml";
 
 	@Override
-	public void generate(File file) {
+	public void generate(File targetFile) {
 		System.out.println("クラス図を生成します。");
-		System.out.println("対象ファイル名：" + file.getName());
+		System.out.println("対象ファイル名：" + targetFile.getName());
 
-		File createdPumlFile = createPumlFile(file);
+		// PUMLファイルを生成する。
+		File createdPumlFile = createPumlFile(targetFile);
+		// 対象ファイルから出力コレクションを生成する。
+		Collection<?> lines = createLines(targetFile);
+		// PUMLファイルに出力コレクションを書き込む
+		write(createdPumlFile, lines);
 
 		System.out.println("クラス図を生成しました。");
 	}
 
 	/**
+	 * <PRE>
 	 * 対象ファイルを基にPUMLファイルを生成する。
-	 * @param file
+	 * TODO 未実装
+	 * </PRE>
+	 *
+	 * @param targetFile
 	 * @return
 	 */
-	private File createPumlFile(File file) {
-		File createFile = new File(FilenameUtils.getBaseName(file.getName())+PUML_EXTENTION);
+	private File createPumlFile(File targetFile) {
+		File createFile = new File(FilenameUtils.getBaseName(targetFile.getName()) + FileConstants.PUML_EXTENTION);
 		if (createFile.exists()) {
 			System.out.println("既に存在するファイルです。ファイル名：" + createFile.getName());
 			return createFile;
 		}
 		System.out.println(createFile.getAbsolutePath());
 		try {
-			if(!createFile.createNewFile()) {
+			if (!createFile.createNewFile()) {
 				System.out.println("ファイルが作成できませんでした。");
 			}
 		} catch (IOException ioe) {
@@ -41,4 +54,36 @@ public class ClassDiagramGenerator implements DiagramGenerator {
 		return createFile;
 	}
 
+	/**
+	 * <PRE>
+	 * 対象ファイルから出力コレクションを生成する。
+	 * TODO 未実装
+	 * </PRE>
+	 *
+	 * @param targetFile
+	 * @return
+	 */
+	private Collection<?> createLines(File targetFile) {
+		Collection<String> lines = new ArrayList<>();
+		lines.add("Sample作ります。");
+		lines.add("ほにゃららら〜");
+		return lines;
+	}
+
+	/**
+	 * <PRE>
+	 * PUMLファイルに出力コレクションを書き込む
+	 * TODO 未実装
+	 * </PRE>
+	 *
+	 * @param createdPumlFile
+	 * @param lines
+	 */
+	private void write(File createdPumlFile, Collection<?> lines) {
+		try {
+			FileUtils.writeLines(createdPumlFile, FileConstants.DEFAULT_CHAR_SET, lines, FileConstants.NOT_APPEND);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
 }
