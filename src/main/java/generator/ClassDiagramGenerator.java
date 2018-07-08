2061a1725294e45270653fd3e386410e1c0dc37f
三浦済 <wataru@miurawatarunoMacBook-Pro.local>
2018-07-08 09:59:31 +0900

    Setting import apache commons.

--- src/main/java/generator/ClassDiagramGenerator.java
+++ src/main/java/generator/ClassDiagramGenerator.java
@@ -8,7 +8,14 @@ public class ClassDiagramGenerator implements DiagramGenerator {
 	public void generate(File file) {
 		System.out.println("クラス図を生成します。");
 		System.out.println("対象ファイル名：" + file.getName());
+		try {
+			System.out.println(file.getAbsolutePath());
+		} catch (Exception e) {
 
+		}
+
+
+		System.out.println("クラス図を生成しました。");
 	}
 
 }
